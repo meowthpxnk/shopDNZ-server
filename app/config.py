f6bd4af21641bd98e4b422c5eadce17b7df7e606
Meowthpxnk <meowthpxnk@gmail.com>
2023-02-11 23:10:56 +0300

    commited

--- app/config.py
+++ app/config.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config():
-    TG_API_TOKEN = os.getenv("TG_API_TOKEN")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
     UPLOAD_FOLDER = 'uploads'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
